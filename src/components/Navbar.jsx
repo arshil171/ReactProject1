d2cb7bfede01797dbaa9ea3c0c458b58875dc5c1
HappyOP411 <harpalkhachar9@gmail.com>
2025-06-23 17:07:59 +0530

    version1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
 
 
 const Navbar = () => {
   return (
-    <div  className='flex relative'>
+    <div  className='flex relative border h-[70px] items-center bg-black text-white'>
       <div><img src="" alt="" /></div>
-      <div className='flex absolute  top-[20px] right-[170px] '>
-        <ul className='flex border'>
+      <div className='flex absolute  h-[70px] left-[25%] w-[45%]  items-center justify-between '>
+        <ul className='flex  w-[100%] justify-between'>
           <li>Home</li>
           <li>Products</li>
           <li>Features</li>
@@ -16,12 +17,12 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className=' absolute right-[130px] top-[20px]'>
+      <div className=' absolute right-[15%] '>
           <input type="text" placeholder='Search' className='border rounded-[20px] pl-[20px] h-[35px] bg-white text-black' />
-         <div className='absolute top-[10px] right-[30px]'><FaSearch /></div>
+         <div className='absolute top-[10px] right-[20px]  text-black'><FaSearch /></div>
       </div>
 
-      <button>Dark</button>
+      <button className='absolute right-[10%] text-[25px] cursor-pointer'><FaMoon /></button>
     </div>
   )
 }
